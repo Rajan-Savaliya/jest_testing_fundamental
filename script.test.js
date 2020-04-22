caf8c7c62ddf291bdbbec2fa58201c8f8cf8501b
rajan savaliya <rajansavaliya6328@gmail.com>
2020-04-22 16:07:56 +0530

    add to github

---
+++ script.test.js
@@ -0,0 +1,30 @@
+const googleSearch = require('./script');
+
+dbMock = [
+  'dogs.com',
+  'cheesepuff.com',
+  'disney.com',
+  'dogpictures.com',
+];
+
+console.log(googleSearch('dog', dbMock));
+
+it('silly test', () => {
+  expect('hello').toBe('hello');
+});
+it('is running', () => {
+  expect(googleSearch('dog', dbMock)).toExist; 
+});
+
+it('is is working', () => {
+  expect(googleSearch('dog', dbMock)).toEqual(['dogs.com', 'dogpictures.com'])
+});
+
+it('works with undefined and null input', () => {
+  expect(googleSearch(undefined, dbMock)).toEqual([])
+  expect(googleSearch(null, dbMock)).toEqual([])
+});
+
+it('does not return more than 3 matches', () => {
+  expect(googleSearch('.com', dbMock).length).toEqual(3)
+});
